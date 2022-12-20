4e924d1deb6b7bba9fe27d2e485189fa85255a87
Ahmed Amin <AhmedAminHassanIsmail@gmail.com>
2022-12-20 22:07:10 +0200

    add theme color with Color extension

---
+++ Cryptocurrency/Cryptocurrency/Extensions/Color+EXT.swift
@@ -0,0 +1,24 @@
+//
+//  Color+EXT.swift
+//  Cryptocurrency
+//
+//  Created by Ahmed Amin on 20/12/2022.
+//
+
+import SwiftUI
+
+
+extension Color {
+    
+    static let theme = ColorTheme()
+}
+
+
+struct ColorTheme {
+    let accent          = Color("AccentColor")
+    let background      = Color("BackgroundColor")
+    let green           = Color("GreenColor")
+    let red             = Color("RedColor")
+    let secondaryText   = Color("SecondaryTextColor")
+}
+
