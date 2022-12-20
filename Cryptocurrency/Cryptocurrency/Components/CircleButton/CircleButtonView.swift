953680204eb911b6a47708ec74df2f99a42f754b
Ahmed Amin <AhmedAminHassanIsmail@gmail.com>
2022-12-20 23:41:47 +0200

    update home header view and add circle button animation

--- Cryptocurrency/Cryptocurrency/Components/CircleButton/CircleButtonView.swift
+++ Cryptocurrency/Cryptocurrency/Components/CircleButton/CircleButtonView.swift
@@ -19,7 +19,7 @@ struct CircleButtonView: View {
                 Circle()
                     .foregroundColor(Color.theme.background)
             )
-            .shadow(color: Color.theme.accent,
+            .shadow(color: Color.theme.accent.opacity(0.25),
                     radius: 10, x: 0, y: 0)
             .padding()
     }
--- Cryptocurrency/Cryptocurrency/Modules/Home/Views/HomeView.swift
+++ Cryptocurrency/Cryptocurrency/Modules/Home/Views/HomeView.swift
@@ -8,20 +8,67 @@
 import SwiftUI
 
 struct HomeView: View {
+    
+    //MARK: - Variables
+    @State private var showPortfolio: Bool = false
+    
+    //MARK: - Body
     var body: some View {
         ZStack {
             // background layer
-            Color.theme.background
-                .ignoresSafeArea()
+            homeBackground
             
             VStack {
-                Text("header")
+                // header View
+                homeHeader
                 
-                Spacer()
+                Spacer(minLength: 0)
             }
         }
     }
 }
+
+
+extension HomeView {
+    
+    //MARK: - Home Background 
+    var homeBackground: some View {
+        Color.theme.background
+            .ignoresSafeArea()
+    }
+    
+    //MARK: - Header View
+    var homeHeader: some View {
+        HStack {
+            CircleButtonView(iconName: showPortfolio ? "plus" : "info")
+                .animation(.none)
+                .background(
+                    CircleButtonAnimationView(animate: $showPortfolio)
+                )
+            
+            
+            Spacer()
+            
+            Text(showPortfolio ? "Portfolio" : "Live Prices")
+                .font(.headline)
+                .fontWeight(.heavy)
+                .foregroundColor(Color.theme.accent)
+                .animation(.none)
+            
+            Spacer()
+            
+            CircleButtonView(iconName: "chevron.right")
+                .rotationEffect(Angle(degrees: showPortfolio ? 180 : 0))
+                .onTapGesture {
+                    withAnimation(.spring()) {
+                        showPortfolio.toggle()
+                    }
+                }
+        }
+        .padding(.horizontal)
+    }
+}
+
 
 struct HomeView_Previews: PreviewProvider {
     static var previews: some View {
