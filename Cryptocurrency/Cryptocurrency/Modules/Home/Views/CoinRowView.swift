1d6e57d8cab00aa3aea8c9010f56298bc9b8c2ac
Ahmed Amin <AhmedAminHassanIsmail@gmail.com>
2022-12-25 17:51:25 +0200

    Design a row to display coins in a List

---
+++ Cryptocurrency/Cryptocurrency/Modules/Home/Views/CoinRowView.swift
@@ -0,0 +1,39 @@
+//
+//  CoinRowView.swift
+//  Cryptocurrency
+//
+//  Created by Ahmed Amin on 21/12/2022.
+//
+
+import SwiftUI
+
+struct CoinRowView: View {
+    
+    let coin: Coin
+    let showHoldingColumn: Bool
+    
+    var body: some View {
+        HStack(spacing: 0) {
+            
+            leftColumn
+            
+            Spacer()
+            
+            if showHoldingColumn {
+                centerColumn
+            }
+            
+            rightColumn
+            
+        }
+        .font(.subheadline)
+    }
+}
+
+struct CoinRowView_Previews: PreviewProvider {
+    static var previews: some View {
+        CoinRowView(coin: dev.coin, showHoldingColumn: true)
+            .previewLayout(.sizeThatFits)
+            .preferredColorScheme(.dark)
+    }
+}
