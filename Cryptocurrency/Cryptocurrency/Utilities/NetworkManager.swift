a64f58c48c6d1bd3e1bf62823ed7d4e8918f7a9f
Ahmed Amin <AhmedAminHassanIsmail@gmail.com>
2022-12-27 01:01:40 +0200

    execute handleURLResponse and add custom error with networking error enum

--- Cryptocurrency/Cryptocurrency/Utilities/NetworkManager.swift
+++ Cryptocurrency/Cryptocurrency/Utilities/NetworkManager.swift
@@ -9,16 +9,37 @@ import Foundation
 import Combine
 
 final class NetworkManager {
-    static func download(url: URL) -> AnyPublisher<Data, Error> {
-   return URLSession.shared.dataTaskPublisher(for: url)
-            .subscribe(on: DispatchQueue.global(qos: .default))
-            .tryMap { (output) -> Data in
-                guard let response = output.response as? HTTPURLResponse,
-                      response.statusCode >= 200 && response.statusCode < 300 else { throw URLError(.badServerResponse)}
-                return output.data
+    
+    enum NetworkingError: LocalizedError {
+        case badURLResponse(url: URL)
+        case unknown
+        
+        var errorDescription: String? {
+            switch self {
+            case .badURLResponse(let url):
+                return "[🔥] Bad response from URL: \(url)"
+                
+            case .unknown:
+                return "[⚠️] Unknown error occured"
             }
+        }
+    }
+    
+    
+    static func download(url: URL) -> AnyPublisher<Data, Error> {
+        return URLSession.shared.dataTaskPublisher(for: url)
+            .subscribe(on: DispatchQueue.global(qos: .default))
+            .tryMap({ try handleURLResponse(output: $0, url: url) })
             .receive(on: DispatchQueue.main)
             .eraseToAnyPublisher()
+    }
+    
+    static func handleURLResponse(output: URLSession.DataTaskPublisher.Output, url: URL) throws -> Data {
+        guard let response = output.response as? HTTPURLResponse,
+              response.statusCode >= 200 && response.statusCode < 300 else {
+            throw  NetworkingError.badURLResponse(url: url)
+        }
+        return output.data
     }
     
     static func handleCompletion(completion:  Subscribers.Completion<Error>) {
