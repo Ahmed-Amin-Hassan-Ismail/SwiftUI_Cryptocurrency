0750e756ff08a20a19c01d6eeb1fd011089e2be6
Ahmed Amin <AhmedAminHassanIsmail@gmail.com>
2023-06-06 18:32:51 +0300

    create XCode Project (initial commit)

---
+++ Cryptocurrency/Cryptocurrency/CryptocurrencyApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptocurrencyApp.swift
+//  Cryptocurrency
+//
+//  Created by Ahmed Amin on 06/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct CryptocurrencyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
