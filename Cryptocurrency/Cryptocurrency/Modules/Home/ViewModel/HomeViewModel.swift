4d307f74644a1c503b6ef86871e8e5858822499c
Ahmed Amin <AhmedAminHassanIsmail@gmail.com>
2023-06-06 18:37:17 +0300

    create home screen swift file

---
+++ Cryptocurrency/Cryptocurrency/Modules/Home/ViewModel/HomeViewModel.swift
@@ -0,0 +1,20 @@
+//
+//  HomeViewModel.swift
+//  Cryptocurrency
+//
+//  Created by Ahmed Amin on 06/06/2023.
+//
+
+import SwiftUI
+
+struct HomeViewModel: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct HomeViewModel_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeViewModel()
+    }
+}
