b82b3047fae80ef11907544a63bcd15ca63bf14f
Ahmed Amin <AhmedAminHassanIsmail@gmail.com>
2023-06-08 23:46:53 +0300

    add filter and sort logic with searchBar

--- Cryptocurrency/Cryptocurrency/Modules/Home/ViewModel/HomeViewModel.swift
+++ Cryptocurrency/Cryptocurrency/Modules/Home/ViewModel/HomeViewModel.swift
@@ -33,12 +33,42 @@ final class HomeViewModel: ObservableObject {
     //MARK: - Methods
     
     private func addSubscriber() {
-        
-        service.$coins
-            .sink { [weak self] coins in
-                guard let self = self else { return }
+         
+        $searchText
+            .combineLatest(service.$coins)
+            .debounce(for: .seconds(0.5), scheduler: RunLoop.main)
+            .map(filterCoins)
+            .sink { coins in
                 self.coins = coins
             }
             .store(in: &cancellables)
+    }
+}
+
+//MARK: - Search and Sort
+
+extension HomeViewModel {
+    
+    private func filterCoins(text: String, coins: [Coin]?) -> [Coin]? {
+        
+        guard isSearchBarNoEmpty(with: text) else {
+            return coins
+        }
+        
+        return coins?.filter({
+            startFilter(with: text, coinValue: $0.name) ||
+            startFilter(with: text, coinValue: $0.symbol) ||
+            startFilter(with: text, coinValue: $0.id)
+        })
+    }
+    
+    private func isSearchBarNoEmpty(with text: String) -> Bool {
+        
+        return !(text.isEmpty)
+    }
+    
+    private func startFilter(with searchText: String, coinValue: String) -> Bool {
+        
+        coinValue.lowercased().contains(searchText.lowercased())
     }
 }
